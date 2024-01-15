375e5936bc7c3f99d57d1b2440c174244bae1706
Tair2033 <tairenikeev@gmail.com>
2024-01-15 10:55:58 +0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,54 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <main>1</main>
   <router-view />
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { defineComponent } from "vue";
+import store from "./store";
 
-nav {
-  padding: 30px;
+const client_id = "65406aaaa2db441b8d4559e0ffd30852";
+const redirect_uri = "http://localhost:8080";
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+const authLink = `https://accounts.spotify.com/authorize?client_id=${client_id}&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=${redirect_uri}`;
 
-    &.router-link-exact-active {
-      color: #42b983;
+export default defineComponent({
+  data() {
+    return {};
+  },
+  mounted() {
+    const hashParams = {
+      access_token: String,
+    };
+
+    const r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    let e;
+
+    while ((e = r.exec(q))) {
+      // @ts-expect-error: Unreachable code error
+      hashParams[e[1]] = decodeURIComponent(e[2]);
     }
-  }
+
+    if (!hashParams.access_token) {
+      window.location.href = authLink;
+    } else {
+      console.log(hashParams.access_token);
+
+      //this.props.setToken(hashParams.access_token);
+      store.dispatch("fetchUser", hashParams.access_token);
+    }
+  },
+});
+</script>
+
+<style lang="scss">
+@import url("normalize.css");
+@import url("https://fonts.googleapis.com/css2?family=Comfortaa:wght@400;500;700&display=swap");
+
+body {
+  background-color: black;
+  font-family: "Comfortaa", sans-serif;
+  color: white;
 }
 </style>
